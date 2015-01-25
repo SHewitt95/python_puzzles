0bba8819498f8a54e7726c0c6d586420ebe343ba
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-01-24 23:05:56 -0500

    Added comments

--- 06_sum_square_difference.py
+++ 06_sum_square_difference.py
@@ -1,15 +1,22 @@
-def sum_of_squares(max_n):
-    return sum([i**2 for i in range(1, max_n+1)])
+'''
+Puzzle: Find the difference between the sum of the squares of 
+the first one hundred natural numbers and the square of the sum
+of the first hundred natural numbers.
+'''
+def sum_of_squares(numbers):
+    return sum([i**2 for i in range(1, numbers+1)])
 
-def square_of_sum(max_n):
-    return sum(range(1, max_n+1)) ** 2
+def square_of_sum(numbers):
+    return sum(range(1, numbers+1)) ** 2
 
-print(square_of_sum(100) - sum_of_squares(100))
+number_of_numbers = 100
+print(square_of_sum(number_of_numbers) - sum_of_squares(number_of_numbers))
 
 '''
 Equation found at this site:
 http://codereview.stackexchange.com/questions/
 58460/sum-of-squares-square-of-sum-difference
-'''
+
 max = 100
 print(((max-1)*(max)*(max+1)*(3*max + 2))/12)
+'''
