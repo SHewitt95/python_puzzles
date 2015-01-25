ec9474630c3d00ff61a7a55e4d49701c4ee5e7e0
SHewitt95 <SHewitt95@users.noreply.github.com>
2015-01-24 22:19:10 -0500

    Added comments

--- 02_even_fib_numbers_sum.py
+++ 02_even_fib_numbers_sum.py
@@ -1,11 +1,16 @@
-even_sum = 0
-fib = 0
+'''
+By considering the terms in the Fibonacci 
+sequence whose values do not exceed four million, 
+find the sum of the even-valued terms.
+'''
+even_sum = 0 # Sum of even-valued terms.
+fib = 0 # Current Fibonacci number
 number1 = 1
 number2 = 0
 while (fib < 4000000):
-    fib = number1 + number2
-    number1 = number2
-    number2 = fib
+    fib = number1 + number2 # Calculates fib number
+    number1 = number2 # Moves lesser fib sequence number forward
+    number2 = fib # Moves greater fib sequence number forward
     if (fib % 2 == 0):
         even_sum = even_sum + fib
 
