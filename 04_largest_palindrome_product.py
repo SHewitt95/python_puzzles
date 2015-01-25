9d516c7c41340ceb0394cfaeaa8fe9d0410722fa
SHewitt95 <SHewitt95@users.noreply.github.com>
2015-01-24 22:30:24 -0500

    Added comments

--- 04_largest_palindrome_product.py
+++ 04_largest_palindrome_product.py
@@ -1,3 +1,6 @@
+'''
+Puzzle: Find the largest palindrome made from the product of two 3-digit numbers.
+'''
 def is_palindrome(n):
     ''' Checks if number n is a palindrome '''
     number_string = str(n)
@@ -20,13 +23,13 @@ largest_number = 0
 largest_digit_number = 999
 smallest_digit_number = 100
 
-for i in range(largest_digit_number, smallest_digit_number, -1):
-    for j in range(largest_digit_number, smallest_digit_number, -1):
+for i in range(largest_digit_number, smallest_digit_number, -1): # Establishes multiplicand
+    for j in range(largest_digit_number, smallest_digit_number, -1): # Establishes multiplier
         product = i * j
         if (is_palindrome(product) and product > largest_number):
             largest_number = product
             
-print(largest_number)
+print(largest_number) # Prints answer
     
     
     
