7b1763bd4d839bf561951537bda8e7c60c7a0327
SHewitt95 <SHewitt95@users.noreply.github.com>
2015-01-24 22:12:46 -0500

    Added comments

--- 01_multiples_three_five.py
+++ 01_multiples_three_five.py
@@ -1,7 +1,12 @@
+'''
+Puzzle: Find the sum of all the multiples of 3 or 5 below 1000.
+'''
 sum = 0
 max_number = 999
 
-for i in range(max_number, 0, -1):
+for i in range(max_number, 0, -1): # Counts down from max_number to 0.
+
+    # If the number (i) is divisible by 3 or 5, increment sum by i.
     if (i % 3 == 0 or i % 5 == 0):
         sum += i
 
